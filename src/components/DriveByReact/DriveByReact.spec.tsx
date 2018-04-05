84b0550e399d377898231c3e28b5a08fb694546d
Samuel Li <samli@samuelli.net>
2018-04-06 09:55:38 +1000

    refactored to move main component out of App.

---
+++ src/components/DriveByReact/DriveByReact.spec.tsx
@@ -0,0 +1,135 @@
+import * as React from 'react';
+import DriveByReact, { State } from './DriveByReact';
+import { DraggableId, DraggableLocation, DropResult } from 'react-beautiful-dnd';
+import { mount, shallow } from 'enzyme';
+import {
+  DROPPABLE_ID as InstructionQueueDroppableId
+} from '../InstructionQueueContainer/InstructionQueueContainer';
+import {
+  DROPPABLE_ID as InstructionSourceDroppableId
+} from '../InstructionSourceContainer/InstructionSourceContainer';
+import { drive } from '../../services/SBrickService/SBrickService';
+
+jest.mock('../../services/SBrickService/SBrickService', () => ({
+  'drive': jest.fn().mockImplementation(() => Promise.resolve({}))
+}));
+
+const makeInstruction = (index: number) => ({
+  displayName: `displayName${index}`,
+  id: `instruction-${index}`,
+  sBrickCommand: index % 3
+});
+
+const buildDropResultFor = (draggableId: DraggableId,
+                            source: DraggableLocation,
+                            destination: DraggableLocation | null): DropResult => ({
+  draggableId: draggableId,
+  type: 'type',
+  source: source,
+  destination: destination,
+  reason: 'DROP'
+});
+
+const makeInstructions = (n: number) => {
+  return Array.from({length: n}, (v, i) => i).map((i) => makeInstruction(i));
+};
+
+describe('App', function () {
+  beforeEach(() => {
+    // drive.mockClear();
+  });
+
+  it('should add instruction to InstructionQueue when InstructionQueus is empty', () => {
+    const state: State = {
+      instructionSource: makeInstructions(2),
+      instructionQueue: []
+    };
+    const result: DropResult = buildDropResultFor(
+      'instruction-0',
+      {
+        droppableId: InstructionSourceDroppableId,
+        index: 0
+      },
+      {
+        droppableId: InstructionQueueDroppableId,
+        index: 0
+      }
+    );
+
+    const wrapper = mount(<DriveByReact/>);
+    wrapper.setState(state);
+    wrapper.instance().onDragEnd(result);
+
+    const updatedInstructionQUeue = wrapper.state('instructionQueue');
+    expect(updatedInstructionQUeue.length).toBe(1);
+    expect(updatedInstructionQUeue[0].id).toBe('instruction-0');
+  });
+
+  it('should reorder instructions in InstructionQueue', () => {
+    const state: State = {
+      instructionSource: [],
+      instructionQueue: makeInstructions(2)
+    };
+    const result: DropResult = buildDropResultFor(
+      'instruction-0',
+      {
+        droppableId: InstructionQueueDroppableId,
+        index: 0
+      },
+      {
+        droppableId: InstructionQueueDroppableId,
+        index: 1
+      }
+    );
+
+    const wrapper = mount(<DriveByReact/>);
+    wrapper.setState(state);
+    wrapper.instance().onDragEnd(result);
+
+    const updatedInstructionQueue = wrapper.state('instructionQueue');
+    expect(updatedInstructionQueue.length).toBe(2);
+    expect(updatedInstructionQueue[0].id).toBe('instruction-1');
+    expect(updatedInstructionQueue[1].id).toBe('instruction-0');
+  });
+
+  it('should remove instruction from InstructionQueue', () => {
+    const state: State = {
+      instructionSource: [],
+      instructionQueue: makeInstructions(2)
+    };
+    const result: DropResult = buildDropResultFor(
+      'instruction-0',
+      {
+        droppableId: InstructionQueueDroppableId,
+        index: 0
+      },
+      null
+    );
+
+    const wrapper = mount(<DriveByReact/>);
+    wrapper.setState(state);
+    wrapper.instance().onDragEnd(result);
+
+    const updatedInstructionQueue = wrapper.state('instructionQueue');
+    expect(updatedInstructionQueue.length).toBe(1);
+    expect(updatedInstructionQueue[0].id).toBe('instruction-1');
+  });
+
+  it('should send instructions to backend in the correct order', (done) => {
+    const state: State = {
+      instructionSource: [],
+      instructionQueue: makeInstructions(3)
+    };
+
+    const onInstructionExecuted = (result: any) => {
+      expect(drive).toHaveBeenCalledTimes(3);
+      // @ts-ignore tslint doesn't know that drive is a mock now
+      expect(drive.mock.calls).toEqual([[0], [1], [2]]);
+      done();
+    };
+
+    const wrapper = shallow(<DriveByReact onInstructionsExecuted={onInstructionExecuted}/>);
+    wrapper.setState(state);
+    wrapper.find('.play-btn').simulate('click');
+  });
+});
---
+++ src/components/DriveByReact/DriveByReact.tsx
@@ -0,0 +1,205 @@
+import * as React from 'react';
+import { Component } from 'react';
+import { DragDropContext, DragStart, DropResult } from 'react-beautiful-dnd';
+import { Instruction } from '../../models/Instruction';
+import InstructionSourceContainer, {
+  DROPPABLE_ID as InstructionSourceContainerDroppableId
+} from '../InstructionSourceContainer/InstructionSourceContainer';
+import InstructionQueueContainer, {
+  DROPPABLE_ID as InstructionQueueContainerDroppableId
+} from '../InstructionQueueContainer/InstructionQueueContainer';
+import { SBrickCommand } from '../../models/SBrickCommand';
+import { AsyncFunction, series } from 'async';
+import { insert, remove, reorder } from '../../commons/ListUtils';
+import { drive } from '../../services/SBrickService/SBrickService';
+import InstructionWidget from '../InstructionWidget/InstructionWidget';
+import InstructionSourceWidget from '../InstructionSourceWidget/InstructionSourceWidget';
+
+interface DriveByReactProps {
+  onInstructionsExecuted?: (result: any) => void;
+}
+
+export interface State {
+  instructionSource: Instruction[];
+  instructionQueue: Instruction[];
+  status?: string;
+}
+
+// const grid = 8;
+
+// const getListStyle = (isDraggingOver: boolean): object => ({
+//     background: isDraggingOver ? 'lightblue' : 'lightgrey',
+//     padding: grid,
+//     width: 250
+// });
+const getInstructionSource = (): Instruction[] =>
+  Array.from(
+    [
+      SBrickCommand.forward, SBrickCommand.backward, SBrickCommand.left, SBrickCommand.right],
+    (cmd, index) => ({
+      id: `instruction-${SBrickCommand[cmd]}`,
+      index: index,
+      displayName: SBrickCommand[cmd],
+      sBrickCommand: cmd
+    })
+  );
+
+export default class DriveByReact extends Component<DriveByReactProps, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      instructionSource: getInstructionSource(),
+      instructionQueue: [],
+      status: 'unknown'
+    };
+    this.playInstructionsInQueue = this.playInstructionsInQueue.bind(this);
+  }
+
+  onDragStart = (start: DragStart) => {
+    //
+  }
+  onDragUpdate = () => {
+    //
+  }
+  onDragEnd = (result: DropResult) => {
+    this.updateStatus(result);
+
+    if (this.isDraggedFromSrcToDest(
+      result,
+      InstructionSourceContainerDroppableId,
+      InstructionQueueContainerDroppableId)) {
+      this.addInstructionToInstructionQueue(result);
+    }
+
+    if (this.isDraggedFromSrcToDest(
+      result,
+      InstructionQueueContainerDroppableId,
+      InstructionQueueContainerDroppableId)) {
+      this.reorderInstructionsWithinInstructionQueue(result);
+    }
+
+    if (this.isDraggedFromSrcToNowhere(
+      result,
+      InstructionQueueContainerDroppableId)) {
+      this.removeInstructionFromIntructionQueue(result);
+    }
+  }
+
+  reorderInstructionsWithinInstructionQueue(result: DropResult) {
+    this.setState({
+      instructionQueue: reorder(this.state.instructionQueue, result.source.index, result.destination!.index)
+    });
+  }
+
+  removeInstructionFromIntructionQueue(result: DropResult) {
+    this.setState({
+      instructionQueue: remove(this.state.instructionQueue, result.source.index)
+    });
+  }
+
+  isDraggedFromSrcToNowhere(result: DropResult, srcDroppableId: string) {
+    return result && result.source && !result.destination &&
+      result.source.droppableId === srcDroppableId;
+  }
+
+  isDraggedFromSrcToDest(result: DropResult, srcDroppableId: string, destDroppableId: string) {
+    return result && result.source && result.destination &&
+      result.source.droppableId === srcDroppableId &&
+      result.destination.droppableId === destDroppableId;
+  }
+
+  addInstructionToInstructionQueue(result: DropResult) {
+    const instructionSourceDragged = this.state.instructionSource[result.source.index];
+    const newInstruction: Instruction = {
+      displayName: instructionSourceDragged.displayName,
+      id: `instruction-${this.state.instructionQueue.length}`,
+      sBrickCommand: instructionSourceDragged.sBrickCommand
+    };
+    this.setState({
+      // "!" is non-null assertion operator, telling transpiler to be relaxed about the null-check
+      instructionQueue: insert(this.state.instructionQueue, result.destination!.index, newInstruction)
+    });
+  }
+
+  updateStatus(result: DropResult): void {
+    this.setState({
+      status: `onDragEnd ${result.draggableId}
+                src: ${`${result.source.droppableId}:${result.source.index}`} 
+                dest: ${result.destination ?
+        `${result.destination.droppableId}:${result.destination.index}` :
+        'no-dest'}`
+    });
+  }
+
+  buildInstructionSourceContent = () => {
+    return this.state.instructionSource.map((instructionSource, index) => {
+      return (
+        <InstructionSourceWidget key={index} index={index} instruction={instructionSource}/>
+      );
+    });
+  }
+
+  buildInstructionDroppables = () => {
+    return this.state.instructionQueue.map((instruction, index) => {
+      return (
+        <InstructionWidget key={index} index={index} instruction={instruction}/>
+      );
+    });
+  }
+
+  insertInstructionIntoExecutionQueue = () => {
+    type asyncCallbackType = (err: any, data: any) => void;
+
+    // let executionQueue: ((callback: asyncCallbackType) => void)[] = [];
+    let executionQueue: AsyncFunction<any, any>[] = [];
+    this.state.instructionQueue.forEach((instruction) => {
+      const executor: (callback: asyncCallbackType) => void = (callback) => {
+        drive(instruction.sBrickCommand).then(
+          (resp: any) => callback(null, resp.data)).catch(
+          (err: any) => callback(err, null));
+      };
+      executionQueue.push(executor);
+    });
+    return executionQueue;
+  }
+
+  playInstructionsInQueue = (ev: any) => {
+    const executionQueue = this.insertInstructionIntoExecutionQueue();
+    series(executionQueue, this.props.onInstructionsExecuted);
+  }
+
+  // const getItemStyle = (draggableStyle: any, isDragging: boolean): Object => ({
+  //     // some basic styles to make the items look a bit nicer
+  //     // userSelect: 'none',
+  //     // padding: grid * 2,
+  //
+  //     // change background colour if dragging
+  //     // background: isDragging ? 'lightgreen' : 'grey',
+  //
+  //     // styles we need to apply on draggables
+  //     ...draggableStyle,
+  //
+  //     // margin: draggableStyle && draggableStyle.margin ? draggableStyle.margin : `0 0 ${grid}px 0`,
+  // });
+
+  render() {
+    return (
+      <DragDropContext
+        onDragStart={this.onDragStart}
+        onDragUpdate={this.onDragUpdate}
+        onDragEnd={this.onDragEnd}
+      >
+        <div className="app-container">
+          <InstructionSourceContainer>
+            {this.buildInstructionSourceContent()}
+          </InstructionSourceContainer>
+          <InstructionQueueContainer instructions={this.state.instructionQueue}>
+            {this.buildInstructionDroppables()}
+          </InstructionQueueContainer>
+        </div>
+        <div>{this.state.status}</div>
+        <button className="play-btn" onClick={this.playInstructionsInQueue}>Play</button>
+      </DragDropContext>
+    );
+  }
+}
