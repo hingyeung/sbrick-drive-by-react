ba35ef02a5a99afca1038c683096d0b4e0e20bf5
Samuel Li <samli@samuelli.net>
2018-04-17 20:59:51 +1000

    arranged buttons side-by-side in control panel

--- src/components/ControlPanel/ControlPanel.tsx
+++ src/components/ControlPanel/ControlPanel.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import LayoutRow from '../layout/LayoutRow/LayoutRow';
+import LayoutCell from '../layout/LayoutCell/LayoutCell';
+import LayoutContainer from '../layout/LayoutContainer/LayoutContainer';
 
 export interface ControlPanelProps {
   onPlayClick: (event: any) => void;
@@ -7,9 +10,15 @@ export interface ControlPanelProps {
 
 export default (props: ControlPanelProps) => {
   return (
-    <div>
-      <button className="control-container__play-btn btn" onClick={props.onPlayClick}>Play</button>
-      <button className="control-container__clear-btn btn" onClick={props.onClearCLick}>Clear</button>
-    </div>
+    <LayoutContainer>
+      <LayoutRow>
+        <LayoutCell sm={6}>
+          <button className="control-container__play-btn btn" onClick={props.onPlayClick}>Play</button>
+        </LayoutCell>
+        <LayoutCell sm={6}>
+          <button className="control-container__clear-btn btn" onClick={props.onClearCLick}>Clear</button>
+        </LayoutCell>
+      </LayoutRow>
+    </LayoutContainer>
   );
 };
--- src/components/layout/LayoutCell/LayoutCell.tsx
+++ src/components/layout/LayoutCell/LayoutCell.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as classNames from 'classnames';
 
 export interface Props {
-  extraClassNames: string;
+  extraClassNames?: string;
   sm?: number;
   md?: number;
   lg?: number;
